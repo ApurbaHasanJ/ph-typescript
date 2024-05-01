acea887e7164ffc3e57de4035ef04c1d2302ecf8
apurbahasanj <apurbahasanj@gmail.com>
2024-05-01 20:51:11 +0600

    done

--- module1/src/1.11.ts
+++ module1/src/1.11.ts
@@ -1,9 +1,41 @@
 {
+  // Ternary operator || Optional chaining || Nullish Coalescing
 
-    // Ternary operator || Optional chaining || Nullish Coalescing
+  const age: number = 19;
 
+  const isAdult = age >= 18 ? "adult" : "not adult";
+  // console.log({isAdult})
 
-    const age: number = 15
+  // Nullish Coalescing operator
 
+  // null / undefined -> decision making
 
+  const isAuthenticated = "";
+
+  const result1 = isAuthenticated ?? "guest";
+
+  const result2 = isAuthenticated ? isAuthenticated : "Guest";
+//   console.log({ result1 }, { result2 });
+
+  type User = {
+    name: string;
+    address: {
+      city: string;
+      road: string;
+      presentAddress: string;
+      permanentAddress?: string;
+    };
+  };
+
+  const user: User = {
+    name: "Zeesan",
+    address: {
+      city: "NK",
+      road: "maijdee",
+      presentAddress: "maijdee town",
+    },
+  };
+
+  const permanentAddress = user?.address?.permanentAddress ?? "No Permanent Address"
+  console.log({ permanentAddress });
 }
